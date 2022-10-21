5ffa55de5ea421aa26f9b243ff49f78f3d9f7f78
Michael Hirschmugl <michael.hirschmugl@gmx.at>
2022-10-21 11:42:45 +0200

    Working.

--- update_ip.py
+++ update_ip.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python
 
-# This script is an adaption of Jeremy Blythe's script (http://jeremyblythe.blogspot.co.uk/2012/05/python-freedns-client-on-raspberry-pi.html)
+# This script is an adaption of Paul Castle's script, which is the adaption of Jeremy Blythe's script (http://jeremyblythe.blogspot.co.uk/2012/05/python-freedns-client-on-raspberry-pi.html)
 # It will update FreeDNS using multiple keys if you have "Link updates of the same IP together" Disabled.
 
-import urllib2
+import urllib.request as urllib2
+import requests
 import os.path
 
+#SUBDOMAIN = ""
+#USER = ""
+#PASS = ""
 FREEDNS_URL = 'http://freedns.afraid.org/dynamic/update.php?'
-OLDIP_FILE = '/var/lib/misc/oldip'
-USER_KEYS = ["", "", ""]
+USER_KEY = ""
 
-def updatedns(ip):
-    for key in USER_KEYS:
-        print urllib2.urlopen(FREEDNS_URL+key).read().strip()
+print(urllib2.urlopen(FREEDNS_URL+USER_KEY).read().strip())
 
-    f = open(OLDIP_FILE, 'w')
-    f.write(ip)
-    f.close()
+#newip = str(urllib2.urlopen("http://ip.dnsexit.com/").read().strip(), "UTF-8")
+#print(newip)
 
-newip = urllib2.urlopen("http://ip.dnsexit.com/").read().strip()
+#URL = "http://" + USER + ":" + PASS + "@freedns.afraid.org/nic/update?hostname=" + SUBDOMAIN + "&myip=" + newip
 
-if not os.path.exists(OLDIP_FILE):
-    updatedns(newip)
-else:
-    f = open(OLDIP_FILE, 'r')
-    oldip = f.read()
-    f.close()
-    if oldip != newip:
-        updatedns(newip)
+#print(URL)
+
+#urllib2.urlopen(URL)
+#f = requests.get(URL)
+#print(f.text)
